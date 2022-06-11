c797d4b3241140f3e750e41c63faec5907e77b92
SuperfluousMango <31811866+SuperfluousMango@users.noreply.github.com>
2022-06-10 19:56:53 -0500

    Add custom chart legend on dashboard

--- src/app/dashboard/recent-grouped-transactions/recent-grouped-transactions.component.ts
+++ src/app/dashboard/recent-grouped-transactions/recent-grouped-transactions.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { TransactionService } from '@transactions';
-import { first, map, Observable, tap } from 'rxjs';
+import { first } from 'rxjs';
+
+type transactionGroup = { name: string, value: number };
 
 @Component({
     selector: 'recent-grouped-transactions',
@@ -9,9 +11,15 @@ import { first, map, Observable, tap } from 'rxjs';
 })
 export class RecentGroupedTransactionsComponent {
     loading = true;
-    transactionGroups: { name: string, value: number }[] = [];
+    transactionGroups: transactionGroup[] = [];
+    activeEntries: transactionGroup[] = [];
+    legendData: string[] = [];
+
+    colorSchemeName: string;
 
     constructor(transactionService: TransactionService) {
+        this.colorSchemeName = 'vivid';
+
         const date = new Date(),
             year = date.getFullYear(),
             month = date.getMonth() + 1;
@@ -20,7 +28,19 @@ export class RecentGroupedTransactionsComponent {
             .pipe(first())
             .subscribe(data => {
                 this.transactionGroups = data.map(x => ({ name: x.name, value: x.total }));
+                this.legendData = this.transactionGroups.map(x => `${x.name}: ${x.value}`);
                 this.loading = false;
             });
+    }
+
+    itemHover(event?: transactionGroup | number | undefined): void {
+        if (event === undefined) {
+            this.activeEntries = [];
+            return;
+        }
+
+        const eventAsNumber = Number(event),
+            eventAsItem: transactionGroup = event as transactionGroup;
+        this.activeEntries = [ isNaN(eventAsNumber) ? eventAsItem : this.transactionGroups[eventAsNumber] ];
     }
 }
